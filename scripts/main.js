8b38dd5d70fed211db9cd2e46587d850aa23bbb4
HlebHarshkou2002 <erichkrause2012@tut.by>
2023-01-22 22:38:40 +0300

    add a slider to main block

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,55 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+// Profile Text Animation
+
 setTimeout(function() {
     document.querySelector('.person-profile__inner').style.display = 'block';
  }, 1500);
+
+// Main Slider
+
+const slides = document.querySelectorAll('.slide'),
+      btns_prev = document.querySelectorAll('.btn-prev'),
+      btns_next = document.querySelectorAll('.btn-next');
+
+let index = 0;
+
+const activeSlide = n => {
+    for(slide of slides){
+        slide.classList.remove('active');
+    }
+    slides[n].classList.add('active');
+}
+
+activeSlide(index);
+
+const nextSlide = () => {
+    if(index == slides.length - 1) {
+        index = 0;
+        activeSlide(index);
+    } else {
+        index++;
+        activeSlide(index);
+    }
+}
+
+const prevSlide = () => {
+    if(index == 0) {
+        index = slides.length - 1;
+        activeSlide(index);
+    } else {
+        index--;
+        activeSlide(index);
+    }
+}
+
+for (btn_next of btns_next) {
+    btn_next.addEventListener('click', nextSlide);
+}
+
+for (btn_prev of btns_prev) {
+    btn_prev.addEventListener('click', prevSlide);
+}
+
+setInterval(nextSlide, 15000);
