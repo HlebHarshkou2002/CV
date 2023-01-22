88a3ac6acde3075146656d7568fcb4f1c692c85d
HlebHarshkou2002 <erichkrause2012@tut.by>
2023-01-22 19:24:21 +0300

    add a css animation on main block

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,7 @@
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+setTimeout(function() {
+    document.querySelector('.person-profile__inner').style.display = 'block';
+ }, 1500);
