54775244a8a0777c8ab220e8f6211894d6dfbf9c
HlebHarshkou2002 <erichkrause2012@tut.by>
2023-01-25 23:06:02 +0300

    added a menu burger

--- scripts/main.js
+++ scripts/main.js
@@ -48,3 +48,32 @@ for (btn_prev of btns_prev) {
 }
 
 setInterval(nextSlide, 15000);
+
+
+const iconMenu = document.querySelector('.menu-icon')
+const menuBody = document.querySelector('.nav');
+
+console.log(iconMenu)
+if(iconMenu) {
+    iconMenu.addEventListener("click", (e) => {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle("_active");
+        menuBody.classList.toggle("_active");
+    })
+}
+
+const menuLinks = document.querySelectorAll('.nav__link')
+
+if(menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick() {
+        if(iconMenu.classList.contains("_active")){
+            document.body.classList.remove('_lock');
+            iconMenu.classList.remove("_active");
+            menuBody.classList.remove("_active");
+        }
+    }
+}
